af533aaa1b82dc1d9e4285f5f828724b24082b70
naabrimees888 <43852467+naabrimees888@users.noreply.github.com>
2024-03-05 10:26:22 +0200

    Esimene kodune ülesanne

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import raamat from './kuu-raamat.png';
+import './Raamat.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="raamat">
+      <header className="pealkiri">
+        <h1>Apollo kuu raamat:</h1>
       </header>
+      <div className="raamat-data">
+        <div className="kirjeldus">
+        <p><strong>Pealkiri:</strong> Kuidas aidata lapsi ja noori vaimse tervise probleemide korral</p>
+        <p><strong>Autor:</strong> Dr Pier Bryden, Dr Peter Szatmari</p>
+        <p><strong>Ilmumise aasta:</strong> 2024</p>
+        <p><strong>Kirjeldus:</strong> Selles raamatus jagavad tunnustatud ja kogenud Kanada lastepsühhiaatrid oma teadmisi ja kogemusi, mitte ainult maailma ühe juhtiva lastehaigla psühhiaatritena, vaid ka lapsevanematena. Mis meie lastega õieti toimub ja kuidas vaimse tervise ohumärke juba varakult tuvastada ning lapsi aidata, on selle raamatu peamine eesmärk. Raamatus kirjeldatud lugudes tuginevad autorid oma ühistele arstialastele</p>
+        </div>
+        <img src={raamat} className="raamat-image" alt="Apollo kuu raamat" />
+      </div>
     </div>
   );
 }
